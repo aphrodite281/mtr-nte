dffd14c003a213cdfdb4511aa33873cd8fc16a0f
aphrodite281 <157574121+aphrodite281@users.noreply.github.com>
2024-08-08 19:46:47 +0800

    Update RailMixin.java

--- common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/RailMixin.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/RailMixin.java
@@ -71,7 +71,7 @@ public abstract class RailMixin implements RailExtraSupplier {
     @Inject(method = "<init>(Ljava/util/Map;)V", at = @At("TAIL"), remap = false)
     private void fromMessagePack(Map<String, Value> map, CallbackInfo ci) {
         MessagePackHelper messagePackHelper = new MessagePackHelper(map);
-        modelKey = messagePackHelper.getString("model_key", "");
+        modelKey = messagePackHelper.getString("model_key", "114514");
         isSecondaryDir = messagePackHelper.getBoolean("is_secondary_dir", false);
         verticalCurveRadius = messagePackHelper.getFloat("vertical_curve_radius", 0);
     }
